c5e9835c5c8bf48a5be22c6ac4792e526b15c5c6
Charles Yan <charlesyan@hotmail.com>
2015-07-04 03:52:34 -0400

    changed to use tar_extract

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,5 @@ license          'All rights reserved'
 description      'Installs/Configures chef_cassandra_mesos'
 long_description 'Installs/Configures chef_cassandra_mesos'
 version          '0.1.0'
+
+depends 'tar'
